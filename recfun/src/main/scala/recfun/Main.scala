c52d9d44fb46c2149b4aa00cb9d7a44ac679e62f
Dan Haller <dskhaller@gmail.com>
2014-03-30 12:11:45 +0100

    funsets 2.1

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -42,6 +42,6 @@ object Main {
   def countChange(money: Int, coins: List[Int]): Int = {
     if (money == 0) 1
     else if (money < 0 || coins.isEmpty) 0
-    else countChange(money, coins.tail) + countChange(money-coins.head, coins)
+    else countChange(money-coins.head, coins) + countChange(money, coins.tail)
   }
 }
